542e9de33c6d2914f5c1205c209429c1ff8a0542
John Karahalis <john.karahalis@gmail.com>
2017-09-19 18:20:38 -0400

    Proactively upgrade all NPM packages
    react-router is not upgraded because the 4.x.x branch has a very
    different API and the team has said that they will continue supporting
    3.x.x indefinitely.
    
    striptags is not upgraded because webpack-uglifyjs-plugin doesn't
    support libraries written in ES6 and the striptags team says it's safe
    to stay on the 2.x.x branch of striptags.

--- viewer/core/static/js/app/components/views/chart-hover.js
+++ viewer/core/static/js/app/components/views/chart-hover.js
@@ -8,7 +8,7 @@ import format from 'string-template';
 
 export default class extends React.Component {
   componentDidMount() {
-    let hoverElm = select(this.refs.rect);
+    let hoverElm = select(this.rect);
     this.focusElm = select(`.chart-${this.props.metricId} .focus`);
     this.bisector = d3Array.bisector(d => d.x).left;
 
@@ -20,7 +20,7 @@ export default class extends React.Component {
   }
   _handleMouseMove() {
     let props = this.props;
-    let x0 = props.xScale.invert(mouse(this.refs.rect)[0]);
+    let x0 = props.xScale.invert(mouse(this.rect)[0]);
 
     for (let populationName in props.populations) {
       if (props.populations.hasOwnProperty(populationName)) {
@@ -92,7 +92,7 @@ export default class extends React.Component {
   render() {
     return (
       <rect
-        ref="rect"
+        ref={c => this.rect = c}
         className="hover-zone"
         width={this.props.size.innerWidth}
         height={this.props.size.innerHeight}
