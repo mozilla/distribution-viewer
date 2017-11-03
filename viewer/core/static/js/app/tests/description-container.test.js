2770df1d12c3173a3f48770371d6d3d580e9578a
John Karahalis <john.karahalis@gmail.com>
2017-11-03 17:34:57 -0400

    Fix test failures and warnings

--- viewer/core/static/js/app/tests/description-container.test.js
+++ viewer/core/static/js/app/tests/description-container.test.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { render } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 import DescriptionContainer from '../components/containers/description-container';
+
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('DescriptionContainer', () => {
   it('All tags should be stripped from description before display', () => {
     const description = '<script src="http://example.com/example.js"></script>word1 <b>word2</b> <blockquote>word3</blockquote><br>';
-    const dc = render(<DescriptionContainer rawDescription={description} />);
+    const dc = Enzyme.mount(<DescriptionContainer rawDescription={description} />);
     const descriptionElm = dc.find('.description');
-    expect(descriptionElm.text()).toBe('word1 word2 word3');
+    expect(descriptionElm.html()).toContain('word1 word2 word3');
   });
 
   it('Line breaks should be converted to <br> elements in description', () => {
     const description = 'Mozilla\nbuilds\n\nthe\rFirefox\nbrowser';
-    const dc = render(<DescriptionContainer rawDescription={description} />);
+    const dc = Enzyme.mount(<DescriptionContainer rawDescription={description} />);
     const descriptionElm = dc.find('.description');
-    expect(descriptionElm.html()).toBe('Mozilla<br>\nbuilds<br>\n<br>\nthe<br>\rFirefox<br>\nbrowser');
+    expect(descriptionElm.html()).toContain('Mozilla<br>\nbuilds<br>\n<br>\nthe<br>\nFirefox<br>\nbrowser');
   });
 });
